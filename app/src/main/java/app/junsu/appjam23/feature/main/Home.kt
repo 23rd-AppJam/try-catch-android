4dab77a1211b93f370cd548417937496bd87c4b8
junsuPark <krpjs0508@gmail.com>
2023-08-05 18:32:07 +0900

    home top

--- app/src/main/java/app/junsu/appjam23/feature/main/Home.kt
+++ app/src/main/java/app/junsu/appjam23/feature/main/Home.kt
@@ -1,8 +1,38 @@
 package app.junsu.appjam23.feature.main
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import app.junsu.appjam23.TryCatchTabRow
+import app.junsu.appjam23.TryCatchTopBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Home() {
-
+fun Home(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            TryCatchTopBar(
+                modifier = Modifier.fillMaxWidth(),
+                onNavigationIconClick = navController::navigateUp,
+            )
+        },
+    ) { paddingValues ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+        ) {
+            TryCatchTabRow()
+        }
+    }
 } 
