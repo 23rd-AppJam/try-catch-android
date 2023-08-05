b7ab4a56f20877cc72acd704202a29ff986f2050
junsuPark <krpjs0508@gmail.com>
2023-08-06 02:32:32 +0900

    character

--- app/src/main/java/app/junsu/appjam23/Home.kt
+++ app/src/main/java/app/junsu/appjam23/Home.kt
@@ -1,64 +1,113 @@
 package app.junsu.appjam23
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import app.junsu.appjam23.ui.TodoList
 import app.junsu.appjam23.ui.WeekCalendar
-import app.junsu.appjam23.ui.theme.Green500
+import app.junsu.appjam23.ui.theme.Gray200
+import app.junsu.appjam23.ui.theme.Gray500
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Home(
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(0.4f)
-                    .clip(
-                        RoundedCornerShape(
-                            bottomStart = 30.dp,
-                            bottomEnd = 30.dp,
-                        ),
-                    )
-                    .background(Green500),
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            HomeTopBar(
+                modifier = Modifier.fillMaxWidth(),
             )
+        },
+        containerColor = Gray200,
+    ) { padValues ->
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+                .padding(padValues),
+        ) {
             Column(
-                modifier = modifier.weight(0.6f),
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy((-8).dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                WeekCalendar(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(
-                            start = 20.dp,
-                            end = 20.dp,
-                            top = 25.dp,
-                        ),
+                Image(
+                    painter = painterResource(id = R.drawable.vector_seed),
+                    contentDescription = null,
                 )
-                TodoList(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(
-                            start = 20.dp,
-                            end = 20.dp,
-                            top = 20.dp,
-                        )
+                Image(
+                    painter = painterResource(id = R.drawable.vector_earth),
+                    contentDescription = null,
                 )
             }
+            WeekCalendar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 20.dp,
+                        end = 20.dp,
+                        top = 25.dp,
+                    ),
+            )
+            TodoList(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 20.dp,
+                        end = 20.dp,
+                        top = 20.dp,
+                    )
+            )
+            Spacer(modifier = Modifier.height(20.dp))
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun HomeTopBar(
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        title = {
+            Image(
+                painter = painterResource(id = R.drawable.ic_appjam_logo),
+                contentDescription = null,
+            )
+        },
+        colors = TopAppBarDefaults.smallTopAppBarColors(
+            containerColor = Color.Transparent,
+        ),
+        actions = {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_bell),
+                contentDescription = null,
+                tint = Gray500,
+            )
+            Spacer(
+                modifier = Modifier.width(16.dp),
+            )
+        }
+    )
 }
