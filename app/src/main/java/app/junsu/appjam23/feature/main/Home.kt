c377ce68db140e05d3626e48d7189a7ea1d84975
junsuPark <krpjs0508@gmail.com>
2023-08-05 18:41:17 +0900

    home top app bar

--- app/src/main/java/app/junsu/appjam23/feature/main/Home.kt
+++ app/src/main/java/app/junsu/appjam23/feature/main/Home.kt
@@ -1,16 +1,25 @@
 package app.junsu.appjam23.feature.main
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import app.junsu.appjam23.TryCatchTabRow
-import app.junsu.appjam23.TryCatchTopBar
+import app.junsu.appjam23.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,7 +30,7 @@ fun Home(
     Scaffold(
         modifier = modifier.fillMaxSize(),
         topBar = {
-            TryCatchTopBar(
+            HomeTopBar(
                 modifier = Modifier.fillMaxWidth(),
                 onNavigationIconClick = navController::navigateUp,
             )
@@ -32,7 +41,38 @@ fun Home(
                 .fillMaxSize()
                 .padding(paddingValues),
         ) {
-            TryCatchTabRow()
         }
     }
-} 
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeTopBar(
+    modifier: Modifier = Modifier,
+    onNavigationIconClick: () -> Unit,
+) {
+    TopAppBar(
+        modifier = modifier.fillMaxWidth(),
+        title = {
+            Image(
+                modifier = Modifier.size(
+                    width = 60.dp,
+                    height = 20.dp,
+                ),
+                painter = painterResource(R.drawable.img_logo_dummy),
+                contentDescription = null,
+            )
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+        ),
+        actions = {
+            Spacer(modifier = Modifier.width(8.dp))
+            Icon(
+                painter = painterResource(R.drawable.ic_home_default),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+    )
+}
