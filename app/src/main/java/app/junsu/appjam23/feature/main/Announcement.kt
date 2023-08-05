78543e939713e5094b9a7fe3946b021237367cd6
junsuPark <krpjs0508@gmail.com>
2023-08-05 18:43:33 +0900

    announcement top app bar

--- app/src/main/java/app/junsu/appjam23/feature/main/Announcement.kt
+++ app/src/main/java/app/junsu/appjam23/feature/main/Announcement.kt
@@ -1,8 +1,59 @@
 package app.junsu.appjam23.feature.main
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import app.junsu.appjam23.TryCatchTabRow
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Announcement() {
+fun Announcement(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+) {
+    Scaffold(
+        modifier = modifier.fillMaxSize(),
+        topBar = {
+            AnnouncementTopBar(
+                modifier = Modifier.fillMaxWidth(),
+            )
+        },
+    ) { paddingValues ->
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+        ) {
+            TryCatchTabRow()
+        }
+    }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AnnouncementTopBar(
+    modifier: Modifier = Modifier,
+) {
+    TopAppBar(
+        modifier = modifier.fillMaxWidth(),
+        title = {
+            Text(
+                text = "안내 문자",
+                style = MaterialTheme.typography.titleLarge,
+            )
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+        ),
+    )
 }
