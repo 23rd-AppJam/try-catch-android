ae399227cba4580331757a96d8df7ff7651ddab6
junsuPark <krpjs0508@gmail.com>
2023-08-06 00:25:05 +0900

    layout

--- app/src/main/java/app/junsu/appjam23/MainActivity.kt
+++ app/src/main/java/app/junsu/appjam23/MainActivity.kt
@@ -3,14 +3,20 @@ package app.junsu.appjam23
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import app.junsu.appjam23.ui.theme.Appjam23Theme
+import app.junsu.appjam23.ui.theme.Green500
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppJam23App()
                 }
             }
         }
@@ -30,17 +36,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Appjam23Theme {
-        Greeting("Android")
+private fun AppJam23App() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(0.4f)
+                    .clip(
+                        RoundedCornerShape(
+                            bottomStart = 30.dp,
+                            bottomEnd = 30.dp,
+                        ),
+                    )
+                    .background(Green500),
+            )
+            Box(
+                modifier = Modifier.weight(0.6f),
+            )
+        }
     }
 }
