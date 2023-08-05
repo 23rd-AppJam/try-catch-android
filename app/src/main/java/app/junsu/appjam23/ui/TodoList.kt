21a3c2efe2b813b2cb0d4f14358daeca923670a5
junsuPark <krpjs0508@gmail.com>
2023-08-06 00:44:33 +0900

    check

--- app/src/main/java/app/junsu/appjam23/ui/TodoList.kt
+++ app/src/main/java/app/junsu/appjam23/ui/TodoList.kt
@@ -1,6 +1,7 @@
 package app.junsu.appjam23.ui
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,6 +15,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -22,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import app.junsu.appjam23.R
 import app.junsu.appjam23.ui.theme.Gray500
+import app.junsu.appjam23.ui.theme.Green500
 
 @Composable
 fun TodoList(
@@ -61,15 +67,25 @@ private fun TodoItem(
     modifier: Modifier = Modifier,
     title: String,
 ) {
+    var checked by remember { mutableStateOf(false) }
+
     Row(
         modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.spacedBy(5.dp),
     ) {
         Icon(
-            modifier = Modifier.size(24.dp),
+            modifier = Modifier
+                .size(24.dp)
+                .clickable {
+                    checked = !checked
+                },
             painter = painterResource(id = R.drawable.ic_check),
             contentDescription = "check",
-            tint = Gray500,
+            tint = if (checked) {
+                Green500
+            } else {
+                Gray500
+            },
         )
         Text(
             modifier = Modifier.weight(1f),
