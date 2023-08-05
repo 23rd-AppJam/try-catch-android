a2a02333515e2c9605fd4b4385a7ace90874cb43
junsuPark <krpjs0508@gmail.com>
2023-08-05 18:20:09 +0900

    top appbar

--- app/src/main/java/app/junsu/appjam23/MainActivity.kt
+++ app/src/main/java/app/junsu/appjam23/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -19,6 +22,8 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -77,6 +82,12 @@ fun TryCatchApp() {
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TryCatchTopBar(
+                modifier = Modifier.fillMaxWidth(),
+                onNavigationIconClick = navController::navigateUp,
+            )
+        },
         bottomBar = {
             TryCatchBottomBar(
                 modifier = Modifier.fillMaxWidth(),
@@ -212,4 +223,36 @@ enum class MainSections(
         defaultIconRes = R.drawable.ic_all_default,
         selectedIconRes = R.drawable.ic_all_selected,
     ),
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TryCatchTopBar(
+    modifier: Modifier = Modifier,
+    onNavigationIconClick: () -> Unit,
+) {
+    TopAppBar(
+        modifier = modifier.fillMaxWidth(),
+        title = {
+            Image(
+                modifier = Modifier.size(
+                    width = 60.dp,
+                    height = 20.dp,
+                ),
+                painter = painterResource(R.drawable.img_logo_dummy),
+                contentDescription = null,
+            )
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.background,
+        ),
+        actions = {
+            Spacer(modifier = Modifier.width(8.dp))
+            Icon(
+                painter = painterResource(R.drawable.ic_home_default),
+                contentDescription = null,
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+    )
 }
