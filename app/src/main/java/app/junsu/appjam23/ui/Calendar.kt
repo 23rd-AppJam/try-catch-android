5f9f2abd82db7c18ba8fcc363e8e4ec82c589265
junsuPark <krpjs0508@gmail.com>
2023-08-06 00:11:56 +0900

    calendar

---
+++ app/src/main/java/app/junsu/appjam23/ui/Calendar.kt
@@ -0,0 +1,87 @@
+package app.junsu.appjam23.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import java.time.DayOfWeek
+import java.time.format.TextStyle
+import java.util.Locale
+
+@Composable
+fun WeekCalendar(
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colorScheme.surface)
+            .padding(20.dp),
+    ) {
+        Text(
+            text = "2023년 8월 1일",
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold,
+        )
+        Spacer(modifier = Modifier.height(15.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            DayOfWeek.values().forEachIndexed { index, dayOfWeek ->
+                WeekCalendarDate(
+                    modifier = Modifier.weight(1f),
+                    dayOfWeek = dayOfWeek,
+                    date = index + 6,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun WeekCalendarDate(
+    modifier: Modifier = Modifier,
+    dayOfWeek: DayOfWeek,
+    date: Int,
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREA),
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.outline,
+        )
+
+        Text(
+            text = dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.KOREA),
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun WeekCalendarPreview() {
+    WeekCalendar(
+        modifier = Modifier.padding(horizontal = 16.dp)
+    )
+}
