5d4754314efc9044c21bc8e3e8a52b95ccea973e
junsuPark <krpjs0508@gmail.com>
2023-08-05 18:47:16 +0900

    announcement tab row

--- app/src/main/java/app/junsu/appjam23/feature/main/Announcement.kt
+++ app/src/main/java/app/junsu/appjam23/feature/main/Announcement.kt
@@ -7,13 +7,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import app.junsu.appjam23.TryCatchTabRow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,7 +36,7 @@ fun Announcement(
                 .fillMaxSize()
                 .padding(paddingValues),
         ) {
-            TryCatchTabRow()
+            AnnouncementTabRow()
         }
     }
 }
@@ -56,4 +58,58 @@ fun AnnouncementTopBar(
             containerColor = MaterialTheme.colorScheme.background,
         ),
     )
+}
+
+@Composable
+fun AnnouncementTabRow(
+    modifier: Modifier = Modifier,
+) {
+    TabRow(
+        modifier = modifier.fillMaxWidth(),
+        selectedTabIndex = 0,
+    ) {
+        Tab(
+            selected = true,
+            onClick = { /*TODO*/ },
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 12.dp),
+                text = "전체",
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        }
+
+        Tab(
+            selected = true,
+            onClick = { /*TODO*/ },
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 12.dp),
+                text = "자연재난",
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        }
+
+        Tab(
+            selected = true,
+            onClick = { /*TODO*/ },
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 12.dp),
+                text = "사회재난",
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        }
+
+        Tab(
+            selected = true,
+            onClick = { /*TODO*/ },
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 12.dp),
+                text = "비상재난",
+                style = MaterialTheme.typography.bodyMedium,
+            )
+        }
+    }
 }
